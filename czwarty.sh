74e4020afcac484d4e3e029898ac6622f07c1c2c
Bartosz55245 <dsw55245@student.dsw.edu.pl>
2024-04-25 20:41:21 +0200

    zmodyfikowany

--- czwarty.sh
+++ czwarty.sh
@@ -6,6 +6,8 @@ echo "./pierwszy.sh --date (-d)"
 echo "./drugi.sh --logs (-l)"
 echo "./trzeci.sh --logs liczba(np.30)"
 echo "./czwarty.sh --help (-h)"
+echo "./errorliczba.sh --error liczba(np.30)"
+echo "./klonowanie.sh --init"
 else
 exit 1
 fi
