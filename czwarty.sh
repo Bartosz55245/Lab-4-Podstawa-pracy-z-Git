3c4dd1f6ae4535503834aa09bc95304b301ed513
Bartosz55245 <dsw55245@student.dsw.edu.pl>
2024-04-25 20:15:33 +0200

    rozbudowahelpa

--- czwarty.sh
+++ czwarty.sh
@@ -2,9 +2,10 @@
 
 if [ "$1" == "--help" ]; then
 echo "Wszystkie dostępne opcje: "
-echo "./pierwszy.sh --date"
-echo "./drugi.sh --logs"
+echo "./pierwszy.sh --date (-d)"
+echo "./drugi.sh --logs (-l)"
 echo "./trzeci.sh --logs liczba(np.30)"
+echo "./czwarty.sh --help (-h)"
 else
 exit 1
 fi
